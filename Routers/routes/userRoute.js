fbbe27dbd03532ffa4d333e5153cb3f58a0e3e9a
DeepFried-Butter <koradejayesh@gmail.com>
2025-03-06 08:37:24 +0530

    added separate routes file for routing

---
+++ Routers/routes/userRoute.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const router=express.Router();
+
+router.get('/',(req,res)=>{
+    res.send('User List')
+});
+
+router.post('/',(req,res)=>{
+    res.send('User created')
+})
+
+
+
+module.exports=router;
