1b0e8aed38756f87ba35065cab94d7069615064f
DeepFried-Butter <koradejayesh@gmail.com>
2025-03-06 21:26:21 +0530

    implemented view

--- e-commerce demo/controllers/productController.js
+++ e-commerce demo/controllers/productController.js
@@ -1,22 +1,25 @@
-// GET /products - Fetch all products
+const path = require('path');
+
+
 const getAllProducts = (req, res) => {
-    res.send('Fetching all products');
-  };
-  
-  // POST /products - Add a new product
-  const addProduct = (req, res) => {
-    res.send('Adding a new product');
-  };
-  
-  // GET /products/:id - Fetch a product by its ID
-  const getProductById = (req, res) => {
-    const productId = req.params.id;
-    res.send(`Fetching product with ID: ${productId}`);
-  };
-  
-  // Export the controller functions
-  module.exports = {
-    getAllProducts,
-    addProduct,
-    getProductById,
-  };
+  const filePath = path.join(__dirname, '../views/products.html');
+  res.sendFile(filePath);
+};
+
+// GET /products/:id - Fetch a product by its ID
+const getProductById = (req, res) => {
+  const productId = req.params.id;
+  res.send(`Fetching product with ID: ${productId}`);
+};
+
+// POST /products - Add a new product
+const addProduct = (req, res) => {
+  res.send('Adding a new product');
+};
+
+// Export the controller functions
+module.exports = {
+  getAllProducts,
+  getProductById,
+  addProduct,
+};
