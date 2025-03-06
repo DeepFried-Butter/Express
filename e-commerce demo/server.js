be14ccf7a6220126f406ee10d1e82ae7b2aadaae
DeepFried-Butter <koradejayesh@gmail.com>
2025-03-06 12:04:05 +0530

    crud functionality in e-comm demo

---
+++ e-commerce demo/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Import routes
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+
+// Use routes
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
+app.use('/cart', cartRoutes);
+
+// Start the server on port 4000
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
