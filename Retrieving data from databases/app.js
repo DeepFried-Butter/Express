bb9e9902aa0ee5157027ffb5d9d3a06a4aa1bc0e
DeepFried-Butter <koradejayesh@gmail.com>
2025-03-13 19:44:54 +0530

    implemented sequelize and made changes in product.js and database.js

--- Retrieving data from databases/app.js
+++ Retrieving data from databases/app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -21,5 +22,14 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+sequelize.sync()
+.then(result=>{
+    // console.log(result);
+    app.listen(3000);
+})
+.catch(error=>{
+    console.log(error);
+});
 
 app.listen(3000);
